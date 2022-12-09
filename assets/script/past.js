718e33bdc767039d90bc34d5404e2bd7e7cdd9ff
Satch5918 <satch_elfante@hotmail.com>
2022-12-08 23:08:02 -0300

    agregando JS

---
+++ assets/script/past.js
@@ -0,0 +1,27 @@
+const div3 = document.getElementById('divrow2');
+
+let datos = data.events;
+
+function cards(div,objdato){
+    let stringcard = "";
+
+for (let card of objdato){
+    if(card.date < data.currentDate){
+             stringcard += `<div class="card col-3">
+    <img class="cardimg" src=${card.image} alt="${card.name}">
+    <div class="card-body cartas">
+        <h5 class="card-title">${card.name}</h5>
+        <p class="card-text textocard">${card.description}</p>
+    </div>
+    <div class="card-body bodycard">
+        <p class="card-link textocard">Price: $ ${card.price}</p>
+        <a href="#" class="card-link button">Details</a>
+    </div>
+    </div>`;
+    }
+}
+
+div.innerHTML = stringcard;
+
+}
+cards(div3,datos);
